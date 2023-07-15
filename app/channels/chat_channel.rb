540c6f50bd967899ab6b741aae6e2536bfe07a35
Renzo Tissoni <r.tissoni@gmail.com>
2023-07-15 00:27:21 -0300

    Update channel with dummy message

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -5,12 +5,12 @@ class ChatChannel < ApplicationCable::Channel
 
   def unsubscribed; end
 
-  def receive(data)
-    # Determine actions
-    ActionCable.server.broadcast(current_session, data)
-  end
-
   def respond(data)
-    binding.pry
+    response = "This is a dummy response. Use it  while we figure what to ask ChatGPT about."
+
+    # interviewer_message = Message.create(content: data[:content], actor: :interviewer)
+    # candidate_message = Message.create(content: response, actor: :candidate)
+
+    ActionCable.server.broadcast(current_session, response)
   end
 end
